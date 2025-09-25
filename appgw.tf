529c97a2ca389eb4f3e37322eb09db7d51f19072
raonileite88 <84139231+raonileite88@users.noreply.github.com>
2025-09-25 17:59:22 -0300

    Update appgw.tf

--- appgw.tf
+++ appgw.tf
@@ -1,8 +1,9 @@
+
 resource "azurerm_subnet" "appgw" {
   name                 = "appgw-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "appgw_pip" {
@@ -29,13 +30,40 @@ resource "azurerm_application_gateway" "appgw" {
     subnet_id = azurerm_subnet.appgw.id
   }
 
-  frontend_port {
-    name = "httpPort"
-    port = 80
-  }
-
   frontend_ip_configuration {
     name                 = "appgw-frontend"
     public_ip_address_id = azurerm_public_ip.appgw_pip.id
-  } 
+  }
+
+  frontend_port {
+    name = "frontend-port"
+    port = 80
+  }
+
+  backend_address_pool {
+    name = "default-backend-pool"
+  }
+
+  backend_http_settings {
+    name                  = "default-http-settings"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 20
+  }
+
+  http_listener {
+    name                           = "default-listener"
+    frontend_ip_configuration_name = "appgw-frontend"
+    frontend_port_name             = "frontend-port"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                      = "default-rule"
+    rule_type                 = "Basic"
+    http_listener_name         = "default-listener"
+    backend_address_pool_name  = "default-backend-pool"
+    backend_http_settings_name = "default-http-settings"
+  }
 }
