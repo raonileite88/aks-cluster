58fcebc1533317d6776ade3f8fcab15da501410d
raonileite88 <84139231+raonileite88@users.noreply.github.com>
2025-09-25 17:50:04 -0300

    Create appgw.tf

---
+++ appgw.tf
@@ -0,0 +1,41 @@
+resource "azurerm_subnet" "appgw" {
+  name                 = "appgw-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]  # ajuste se necessário
+}
+
+resource "azurerm_public_ip" "appgw_pip" {
+  name                = "appgw-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_application_gateway" "appgw" {
+  name                = "aksAppGateway"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "appgw-ipcfg"
+    subnet_id = azurerm_subnet.appgw.id
+  }
+
+  frontend_port {
+    name = "httpPort"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "appgw-frontend"
+    public_ip_address_id = azurerm_public_ip.appgw_pip.id
+  } 
+}
