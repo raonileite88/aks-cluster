9a818721cd2891331cc14a6d5567b66d55c751d6
raonileite88 <84139231+raonileite88@users.noreply.github.com>
2025-09-25 17:53:20 -0300

    Update appgw.tf

--- appgw.tf
+++ appgw.tf
@@ -2,7 +2,7 @@ resource "azurerm_subnet" "appgw" {
   name                 = "appgw-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]  # ajuste se necessário
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "appgw_pip" {
